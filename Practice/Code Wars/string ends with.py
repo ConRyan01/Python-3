37b046089ae7ab9e2c465ef747f75b71acdc9279
Connor Ryan <connorryan001@gmail.com>
2023-06-04 22:03:00 -0400

    modified:   Practice/Code Wars/string ends with.py

--- Practice/Code Wars/string ends with.py
+++ Practice/Code Wars/string ends with.py
@@ -1,6 +1,16 @@
 def solution(text, ending):
+    result = None
+    for i in range(len(ending) - 1,-1,-1):
+        j = len(text)-1
+        if ending[i] == text[j]:
+            print(ending[i],text[j])
+            result = True
+            
+        else:
+            print(ending[i],text[j])
+            result = False
+            
+        j-=1
+    return result
 
-    last = len(ending)
-    
-    
-    pass
+print(solution('xmhaomjsakpiqxqilhj','qlqilhj'))
