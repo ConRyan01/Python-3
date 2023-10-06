80b83ab004934b3d5bcf9587fcce9ed3b47362f9
Connor Ryan <101826079+ConRyan01@users.noreply.github.com>
2023-10-06 16:44:20 -0400

    updating udemy projects

--- Udemy Courses/The Python Bible/Hello_You!.py
+++ Udemy Courses/The Python Bible/Hello_You!.py
@@ -15,7 +15,7 @@ enjoy = input('What do you enjoy?: ')
 print(enjoy)
 
 #Create output text
-output = print(f'Hello {name}! you are {age} years old and you live in {city} and you enjoy {enjoy}')
+output = f'Hello {name}! you are {age} years old and you live in {city} and you enjoy {enjoy}'
 
 # alternate string format method: string = "Hello {}! you are {} years old and you live in {} and you enjoy {}"
 #                                 output = string.format(name,age,city,enjoy)
