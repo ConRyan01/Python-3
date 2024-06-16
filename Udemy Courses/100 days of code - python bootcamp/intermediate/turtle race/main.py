aedacf54d60d45dad194272d6c984bb7f0d207f7
Connor Ryan <101826079+ConRyan01@users.noreply.github.com>
2024-06-16 18:05:11 -0400

    finished turtle race project

--- Udemy Courses/100 days of code - python bootcamp/intermediate/turtle race/main.py
+++ Udemy Courses/100 days of code - python bootcamp/intermediate/turtle race/main.py
@@ -1,5 +1,8 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
+is_finished = False
 screen = Screen()
 screen.setup(width=1500, height=400)
 user_bet = screen.textinput(title='make your bet', prompt='which turtle will win the race? Enter a color: ')
@@ -13,6 +16,21 @@ ben = Turtle(shape='turtle')
 dan = Turtle(shape='turtle')
 turtles = [tim, tom, jim, jon, ben, dan]
 
+def draw_finish_line(my_turtles: list[Turtle]):
+    drawing_turtle = my_turtles[0]
+    drawing_turtle.penup()
+    drawing_turtle.goto(700, -200)
+    drawing_turtle.setheading(90)
+    drawing_turtle.pendown()
+    drawing_turtle.pensize(5)
+    while drawing_turtle.ycor() < 200:
+        drawing_turtle.pencolor('red')
+        drawing_turtle.forward(20)
+        drawing_turtle.pencolor('blue')
+        drawing_turtle.forward(20)
+    drawing_turtle.pensize(1)
+    drawing_turtle.setheading(0)
+
 def set_color(turtle: Turtle, color: str):
     turtle.color(color)
 
@@ -22,14 +40,33 @@ def set_start_pos(turtle: Turtle, y_coord: int):
     turtle.goto(x_coord, y_coord)
     turtle.pendown()
 
-for i in range(0, len(turtles)):
-    set_color(turtles[i], colors[i])
+def is_race_finished(is_finished):
+    for turtle in turtles:
+        if turtle.xcor() > 700:
+            winning_color = turtle.pencolor()
+            print(f'{winning_color} wins!')
+            print('you won!') if winning_color == user_bet else print('you lose...')
+            is_finished = True
+            return is_finished
+        
+def move_turtles(turtle: Turtle):
+    rand_distance = random.randint(0,10)
+    turtle.forward(rand_distance)
+
+draw_finish_line(turtles)
 
 y = -150
 for i in range(0, len(turtles)):
+    set_color(turtles[i], colors[i])
     set_start_pos(turtles[i], y)
     y += 50
 
-tim.goto(x=-230, y=0)
+if user_bet:
+    is_race_on = True
+
+while is_race_on and not is_race_finished(is_finished= is_finished):
+
+    for turtle in turtles:
+        move_turtles(turtle)
 
 screen.exitonclick()
